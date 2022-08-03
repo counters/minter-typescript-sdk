7bad4a692c646e889347c5936fc8b70681facb46
webcounters <webcounters@gmail.com>
2022-08-03 17:49:34 +0300

    add gRPC BestTrade

--- src/MinterGrpcApi.ts
+++ src/MinterGrpcApi.ts
@@ -1,7 +1,15 @@
-import { CoinInfoRequest, CoinInfoResponse, AddressRequest, AddressResponse, EstimateCoinSellRequest, EstimateCoinSellResponse } from './proto/resources_pb';
-import { ApiServiceClient } from './proto/api_grpc_pb';
-import { credentials, Metadata } from '@grpc/grpc-js';
-import GrpcOptions from './types/GrpcOptions';
+import {
+  CoinInfoRequest,
+  CoinInfoResponse,
+  AddressRequest,
+  AddressResponse,
+  EstimateCoinSellRequest,
+  EstimateCoinSellResponse,
+  BestTradeRequest, BestTradeResponse
+} from "./proto/resources_pb";
+import { ApiServiceClient } from "./proto/api_grpc_pb";
+import { credentials, Metadata } from "@grpc/grpc-js";
+import GrpcOptions from "./types/GrpcOptions";
 
 export default class MinterGrpcApi {
   private client: ApiServiceClient;
@@ -32,6 +40,14 @@ export default class MinterGrpcApi {
   public estimateCoinSellGrpc(request: EstimateCoinSellRequest, deadline: number | null = null): Promise<EstimateCoinSellResponse> {
     return new Promise<EstimateCoinSellResponse>((resolve, reject) => {
       this.client.estimateCoinSell(request, new Metadata(), this.getOptions(deadline), (err, response) => {
+        if (err) reject(err);
+        else resolve(response);
+      });
+    });
+  }
+  public getBestTradeGrpc(request: BestTradeRequest, deadline: number | null = null): Promise<BestTradeResponse> {
+    return new Promise<BestTradeResponse>((resolve, reject) => {
+      this.client.bestTrade(request, new Metadata(), this.getOptions(deadline), (err, response) => {
         if (err) reject(err);
         else resolve(response);
       });
