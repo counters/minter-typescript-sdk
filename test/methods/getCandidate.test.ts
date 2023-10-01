4930c831750e16c52f1a4750908371c8d5cd62d3
webcounters <webcounters@gmail.com>
2023-10-01 20:39:54 +0300

    add test method for getCandidate

---
+++ test/methods/getCandidate.test.ts
@@ -0,0 +1,20 @@
+import Config from "./../Config";
+import MinterApi from "../../src";
+
+test("MinterApi().getCandidateGrpc()", async () => {
+  const config = new Config();
+  const grpcMinterApi = new MinterApi(config.grpcOptions);
+  const httpMinterApi = new MinterApi(null, config.httpOptions);
+
+  const publicKey = "Mp12345bf7d1c833701ea490c2e77430486922a9fbef713b933cc3b32700f27777"; // HUB
+  const notShowStakes: boolean | null = false;
+  const height: number | null = null;
+  const timeout: number | null = null;
+  const grpcResult = await grpcMinterApi.getCandidateGrpc(publicKey, notShowStakes, height, timeout);
+  // console.info(grpcResult.toObject());
+// return;
+  const httpResult = await httpMinterApi.getCandidateGrpc(publicKey, notShowStakes, height, timeout);
+  console.info(httpResult.toObject());
+  // expect(grpcResult.toObject()).toEqual(httpResult.toObject());
+  // expect(grpcResult.getSymbol()).toEqual(symbol);
+});
